ca6876bd6e241c98d33766c51774909441c8c75e
HuynhNhu1303 <113501498+HuynhNhu1303@users.noreply.github.com>
2023-05-06 18:05:09 +0700

    food

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+    require_once('model/config.php');
+    require_once('model/dbhelper.php');
+?>
+<!-- END HEADR -->
+<main>
+   <div class="container">
+        <div class="row">
+        <?php require "view/header.php"; ?>
+
+
+        <?php require_once('view/home.php'); ?>
+
+
+    
+        </div>
+   </div>
+</main>
+    <?php require_once('view/footer.php'); ?>
+</body>
+
+</html>
